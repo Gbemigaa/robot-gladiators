7f9e1b183f4fa29368bdec600d52566f274cfc66
Ajisegbede Gbenga <ajisegbedegbenga@Ajisegbedes-MacBook-Air.local>
2022-03-21 17:56:07 -0400

    add enemy combatants

--- assets/game.js
+++ assets/game.js
@@ -1,13 +1,28 @@
+//Game States
+//"Win"-Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto","Amy Android","Robo Trumble"]
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+console.log(enemyNames.length)
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
+
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose. ")
@@ -58,4 +73,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert("You need to choose a valid option. Try again!");
   }
 };
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
+  
